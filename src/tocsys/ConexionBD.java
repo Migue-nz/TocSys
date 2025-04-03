31476598d5af9bdefae98087a15ae9c91fb208e5
JGabrielGlez <jegagonzalezes@ittepic.edu.mx>
2025-04-03 01:10:44 -0700

    Servicios busqueda, agregar. Falta darles formato, validar y la de eliminar, actualizar

--- src/tocsys/ConexionBD.java
+++ src/tocsys/ConexionBD.java
@@ -13,7 +13,7 @@ import java.sql.SQLException;
  * @author danie
  */
 public class ConexionBD {
-     private static final String URL = "jdbc:mysql://tangamandapio.software:3306/IngenieriaSoftware";
+     private static final String URL = "jdbc:mysql://tangamandapio.software:3306/pruebas";
     private static final String USUARIO = "admin";
     private static final String CONTRASENA = "admin";
 
